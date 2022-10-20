b466d08d17893fa0300dcc44b17f4ce7719af615
jorge <jorgeaugustokeyboards@gmail.com>
2022-10-20 01:04:50 -0300

    Test sendkeys and click

--- src/test/java/PrincipaisComandosSelenium.java
+++ src/test/java/PrincipaisComandosSelenium.java
@@ -6,14 +6,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class PrincipaisComandosSelenium {
 
     @Test
-   public void testeSeleniumClick(){
+    public void testeSeleniumClick() {
 
         WebDriver driver = new ChromeDriver(); // abrir o browser
-        driver.get("https://www.qaacademy.com.br/");
-        driver.findElement(By.xpath("//p[@id='comp-k7g9c7sd4label']")).click();
+        driver.get("https://www.qaacademy.com.br/"); // abrir o browser
+        driver.findElement(By.xpath("//p[@id='comp-k7g9c7sd2label']")).click();
+        driver.findElement(By.xpath("//span[contains(text(),'Assinar agora')]")).click();
+
+    }
+
+    @Test
+    public void enviarTextosParaTela() {
+
+        WebDriver driver = new ChromeDriver(); // abrir o browser
+        driver.get("https://www.qaacademy.com.br/"); // abrir o browser
+        driver.findElement(By.xpath("//input[@id='input_comp-k37gdip8']")).click();
+        driver.findElement(By.xpath("//input[@id='input_comp-k37gdip8']")).sendKeys("Jorge Augusto Moreira Cezar");
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("jorgeaugustokeyboards@gmail.com");
+        driver.findElement(By.xpath("//input[@id='input_comp-k37gdipq']")).sendKeys("11940003016");
+        driver.findElement(By.xpath("//input[@name='endereço']")).sendKeys("Av. Henriqueta Mendes Guerra , 1330");
+        driver.findElement(By.xpath("//input[@id='input_comp-k37gdiqy']")).sendKeys("Teste Automação Selenium");
+        driver.findElement(By.xpath("//textarea[@id='textarea_comp-k37gdirc']")).sendKeys("Please disregard this email as it is just a Selenium automation test that we are learning in class");
 
 
 
 
-   }
+    }
 }
+
